8a2375ad42f1763394016d8d264348c49084edad
vasanth6190 <vasantharajan@codingmart.com>
2020-10-06 14:49:34 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App(){
+    return (
+      <div>
+          <h1 style={{textAlign:"center"}}>Welcome to my-webpack-react-starter</h1>
+      </div>
+    );
+  };
+
+  export default App
